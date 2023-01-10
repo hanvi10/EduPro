2c08109b061504e1f6b7157eaa362b317e73fcf1
hanvi10 <hanvirgilip@gmail.com>
2023-01-10 18:52:06 +0100

    further changes

--- final-project/app.py
+++ final-project/app.py
@@ -15,9 +15,53 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
-@app.route('/register')
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Get data from register form
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Generate hash
+        hash = generate_password_hash(password)
+        user = db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hash)
+        # Ensure there is username
+        # if not username:
+        #    return apology("must provide username", 400)
+
+        # Ensure there is password
+        #if not password:
+        #    return apology("must provide password", 400)
+
+        # Ensure there is confirmation password
+        #if not confirmation:
+         #   return apology("must provide confirmation password", 400)
+
+        # Ensure password and confirmation password match
+        #if password != confirmation:
+         #   return apology("password does not match confirmation", 400)
+
+        # Ensure that username does not exist
+        #try:
+            # Insert new user to SQL table
+            # user = db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hash)
+        #except:
+          #  return apology("username already in use", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = user
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
